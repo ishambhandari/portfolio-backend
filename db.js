9ea9ef993e15b637f9e666cb0da725f4c7d43b6d
Isham Bhandari <ishambhandari@Ishams-MacBook-Pro.local>
2021-12-18 21:09:29 +0545

    database changes

--- db.js
+++ db.js
@@ -1,12 +1,11 @@
 const Pool = require("pg").Pool;
-require("dotenv").config();
+// require("dotenv").config();
 
-const pool = new Pool({
+const pool = new Pool(process.env.DATABASE_URL, {
   user: process.env.USER,
   password: process.env.PASSWORD,
   database: process.env.DATABASE,
   host: process.env.HOST,
   port: process.env.PORT,
-  databse_url: process.env.DATABASE_URL,
 });
 module.exports = pool;
