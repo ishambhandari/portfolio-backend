ec68302956e0ff1354f00326222785918bb9a500
Isham Bhandari <ishambhandari@Ishams-MacBook-Pro.local>
2021-12-18 20:51:39 +0545

    database config changes

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
 const Pool = require("pg").Pool;
+require("dotenv").config();
 
 const pool = new Pool({
-  user: "postgres",
-  password: "",
-  database: "portfolio",
-  host: "localhost",
-  port: 5432,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  host: process.env.HOST,
+  port: process.env.PORT,
 });
 module.exports = pool;
